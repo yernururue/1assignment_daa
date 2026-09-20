48c6fdd2cdff9e8ffd0b246d3e783ece9b54c891
yernururue <250367@astanait.edu.kz>
2026-09-20 21:22:47 +0500

    feat(quicksort): add randomized three-way partition

--- src/main/java/Algorithms/ThreeWayPartition.java
+++ src/main/java/Algorithms/ThreeWayPartition.java
@@ -1,5 +1,41 @@
 package Algorithms;
 
-// TODO: Implement a reusable three-way partition.
+import Algorithms.metrics.Metrics;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ThreeWayPartition {
+    public static int[] partition(int[] nums, int start, int end, Metrics metrics) {
+        int pivotIndex = ThreadLocalRandom.current().nextInt(start, end + 1);
+        int pivot = nums[pivotIndex];
+
+        int less = start;
+        int current = start;
+        int greater = end;
+
+        while (current <= greater) {
+            metrics.incrementComparisons();
+            if (nums[current] < pivot) {
+                swap(nums, less, current);
+                less++;
+                current++;
+            } else {
+                metrics.incrementComparisons();
+                if (nums[current] > pivot) {
+                    swap(nums, current, greater);
+                    greater--;
+                } else {
+                    current++;
+                }
+            }
+        }
+
+        return new int[]{less, greater};
+    }
+
+    private static void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
 }
