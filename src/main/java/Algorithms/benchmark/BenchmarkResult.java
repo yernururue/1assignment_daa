6930959bbd98b03256abeeb9018813353dd6ecfd
yernururue <250367@astanait.edu.kz>
2026-09-20 21:22:38 +0500

    chore(metrics): add counters API and benchmark result

--- src/main/java/Algorithms/benchmark/BenchmarkResult.java
+++ src/main/java/Algorithms/benchmark/BenchmarkResult.java
@@ -1,5 +1,44 @@
 package Algorithms.benchmark;
 
-// TODO: Represent one benchmark result.
 public class BenchmarkResult {
+    private final String algorithm;
+    private final String input;
+    private final int size;
+    private final double timeMs;
+    private final long comparisons;
+    private final int maxDepth;
+
+    public BenchmarkResult(String algorithm, String input, int size,
+                           double timeMs, long comparisons, int maxDepth) {
+        this.algorithm = algorithm;
+        this.input = input;
+        this.size = size;
+        this.timeMs = timeMs;
+        this.comparisons = comparisons;
+        this.maxDepth = maxDepth;
+    }
+
+    public String getAlgorithm() {
+        return algorithm;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public double getTimeMs() {
+        return timeMs;
+    }
+
+    public long getComparisons() {
+        return comparisons;
+    }
+
+    public int getMaxDepth() {
+        return maxDepth;
+    }
 }
